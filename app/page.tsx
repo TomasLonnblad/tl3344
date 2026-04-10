fbb7b7df54f7f7c6f6c48549cead8fda19ab9612
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-10 12:21:07 +0200

    x

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Component from '@/components/features-section-demo-3'
+
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Component />
+    </main>
+  )
+}
